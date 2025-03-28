bc03eb79b70c146db80b850592c31dc657650ce0
Nithin1729S <sureshnithin1729@gmail.com>
2025-03-28 13:39:56 +0530

    update

--- Application/frontend/src/pages/Index.tsx
+++ Application/frontend/src/pages/Index.tsx
@@ -192,7 +192,7 @@ const Index = () => {
     setIsLoading(true);
     try {
       const response = await fetch(
-        "https://phishing-website-detection-production-42cf.up.railway.app/api/check-phishing",
+        "https://admittedly-usable-jennet.ngrok-free.app/api/check-phishing",
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
@@ -222,7 +222,7 @@ const Index = () => {
         for (let i = 0; i < urls.length; i++) {
           try {
             const response = await fetch(
-              "https://phishing-website-detection-production-42cf.up.railway.app/api/check-phishing",
+              "https://admittedly-usable-jennet.ngrok-free.app/api/check-phishing",
               {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
